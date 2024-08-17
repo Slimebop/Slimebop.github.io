f034d7ae6ad79f7acdd1de1bcacb589dd6014c0a
Slimebop <85127264+Slimebop@users.noreply.github.com>
2024-08-16 18:09:42 -0700

    w

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var gender = ['Masculine', 'Feminine'];
 
 function Random() {
     document.getElementById('tb').innerText = gender.random();
-    print(gender.random());
+    console.log(gender.random());
 }
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
